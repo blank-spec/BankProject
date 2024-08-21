2c238ae2a5f2061de204711695c2644e75ad04d4
blank-spec <164775195+blank-spec@users.noreply.github.com>
2024-08-21 20:45:35 +0300

    Make hasher for password

--- PasswordManager.h
+++ PasswordManager.h
@@ -4,23 +4,39 @@
 #include <conio.h>
 #include "FileManager.h"
 using namespace std;
+
+
+struct HashPassword {
+    size_t operator()(const string& password) const {
+        string reversedPassword = password;
+        reverse(reversedPassword.begin(), reversedPassword.end());
+        auto hash1 = hash<string>{}(reversedPassword);
+        auto hash2 = hash<string>{}(password);
+
+        return hash1 ^ (hash2 + 0x9e3779b9 + (hash1 << 6) + (hash2 << 2));
+    }
+};
+
 //That class manages password
 class PasswordManager {
 private:
     string password;
-    const string passowrdFile = "Password.txt";
+    const string passwordFile = "Password.txt";
+    HashPassword hashPass;
 
     // That function make password, if it doesn't exists
     void makePassword() {
         cout << "You need to create a new password: ";
         cin >> password;
-        FileManager::writeLine(passowrdFile, password);
+        size_t hashedPassword = hashPass(password);
+        FileManager::writeLine(passwordFile, to_string(hashedPassword));
+        password = to_string(hashedPassword);
     }
 
 public:
     // Constructor that defines the password
     PasswordManager() {
-        if (!FileManager::readFile(passowrdFile, password)) {
+        if (!FileManager::readFile(passwordFile, password)) {
             makePassword();
         }
     }
@@ -38,23 +54,29 @@ public:
             makePassword();
         }
 
+        cout << "Enter the password: ";
         string enteredPassword;
         char ch;
-        cout << "Enter the password: ";
-        ch = _getch();
+        while ((ch = _getch()) != 13) { 
+            if (ch == 8) { 
+                if (!enteredPassword.empty()) {
+                    enteredPassword.pop_back();
+                    cout << "\b \b";
+                }
+            }
+            else {
+                enteredPassword += ch;
+                cout << '*';
+            }
+        }
+        cout << endl;
 
-        while (ch != 13) {
-            enteredPassword.push_back(ch);
-            cout << '*';
-            ch = _getch();
-        }
-        if (enteredPassword == password) {
-            cout << " Correct" << endl;
+        size_t hashedPassword = hashPass(enteredPassword);
+
+        
+        if (to_string(hashedPassword) == password) {
+            cout << "Correct" << endl;
             return true;
-        }
-        else {
-            cout << " Incorrect" << endl;
-            return false;
         }
         return false;
     }
